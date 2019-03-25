2df7cd5da5aafc22882df5b8533a6a8b8c48f100
Olga Dakhel <olyapugovi4ka@gmail.com>
2019-03-25 19:26:22 +0300

    Написала метод restart, указала его в двух кейсах, добавила физику для рамки

---
+++ L7_OlgaDakhel/L7_OlgaDakhel/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  L7_OlgaDakhel
+//
+//  Created by MacBook on 19/03/2019.
+//  Copyright © 2019 MacBook. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
